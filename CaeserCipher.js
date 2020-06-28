fc2bc2b98e495d058207cf53c554f63450e6ada5
KamariRoss <kamarimobile@gmail.com>
2020-06-28 08:39:27 -0400

    Taking a break

--- CaeserCipher.js
+++ CaeserCipher.js
@@ -1,4 +1,20 @@
 function caesarCipher(str, num) {
   // shift every number in our given string
   // by the number that is passed in
+
+  //   change all letters to lowercase
+  const lowerCaseString = str.toLowerCase()
+
+  // create alphabet for reference
+  const alphabet = "abcdefghijklmnopqurstuvwxyz".split("")
+  const newString = ""
+
+  for (let i = 0; i < lowerCaseString.length; i++) {
+    const currentLetter = lowerCaseString[i]
+    if (currentLetter === " ") {
+      newString += currentLetter
+      continue
+    }
+  }
 }
+caesarCipher("zoo keeper", 2)
