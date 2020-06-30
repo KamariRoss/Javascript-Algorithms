4969b68bd1b33a224cde56c6c6732cd2b0620759
KamariRoss <kamarimobile@gmail.com>
2020-06-30 11:39:37 -0400

    Finished lesson

--- CaeserCipher.js
+++ CaeserCipher.js
@@ -2,20 +2,27 @@ function caesarCipher(str, num) {
   num = num % 26
 
   // We want all the letters to be the same
-  const lowerCaseString = str.toLowerCase()
+  let lowerCaseString = str.toLowerCase()
 
   // Create a reference for the alphabet
-  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
-  const newString = ""
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+  let newString = ""
 
-  for (const i = 0; i < lowerCaseString.length; i++) {
-    const currentLetter = lowerCaseString[i]
+  // find the current letter
+
+  for (let i = 0; i < lowerCaseString.length; i++) {
+    let currentLetter = lowerCaseString[i]
+    // if there is a space character and add it the the current letter
     if (currentLetter === " ") {
       newString += currentLetter
       continue
     }
-    const currentIndex = alphabet.indexOf(currentLetter)
-    const newIndex = currentIndex + num
+
+    //  Located the current index
+    let currentIndex = alphabet.indexOf(currentLetter)
+    // index of out new letter on our alphabet array
+    let newIndex = currentIndex + num
+    // make sure that the index does go over 25 and loop the beginning instead
     if (newIndex > 25) newIndex = newIndex - 26
     if (newIndex < 0) newIndex = 26 + newIndex
     if (str[i] === str[i].toUpperCase()) {
@@ -23,6 +30,6 @@ function caesarCipher(str, num) {
     } else newString += alphabet[newIndex]
   }
 
-  return newString
+  console.log(newString)
 }
-caesarCipher("Zoo Keeper", 2)
+caesarCipher("Javascript", -900)
